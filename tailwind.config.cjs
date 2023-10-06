153cc1036f8c35bb7e4555d053884355ee62e8c4
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-10-06 16:35:40 -0500

    Instalación de tipografía

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      Crimson_Pro: ['Crimson Pro', 'sans-serif'],
+      Hind_Vadodara: ['Hind Vadodara', 'sans-serif'],
+      Hind_Guntur: ['Hind Guntur', 'sans-serif']
+    },
+    extend: {}
+  },
+  plugins: []
 }
