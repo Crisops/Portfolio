2edd170fb5e409d350851dbe1a4f885959b3f790
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-10-09 12:24:49 -0500

    Se añade menu de navegación interactiva para las diferentes secciones del portfolio

--- src/components/react/LinkMenu.jsx
+++ src/components/react/LinkMenu.jsx
@@ -1,7 +1,43 @@
-const LinkMenu = ({ title = 'Home' }) => {
+import { useEffect, useRef, useState } from 'react'
+
+const LinkMenu = ({ id, title }) => {
+  const link = useRef()
+
+  const [entrySection, setEntrySection] = useState('')
+  const [color, setColor] = useState('#FA3538')
+
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.7
+  }
+
+  const sectionIntersecting = (entries) => {
+    entries.forEach(entry => {
+      const { isIntersecting, target } = entry
+      if (isIntersecting) {
+        setEntrySection(target.id)
+      }
+    })
+  }
+
+  useEffect(() => {
+    const allSection = document.querySelectorAll('section')
+
+    const observer = new IntersectionObserver(sectionIntersecting, options);
+
+    (entrySection === link.current.getAttribute('href').slice(1))
+      ? setColor('#FA3538')
+      : setColor('#898989')
+
+    allSection.forEach(section => {
+      observer.observe(section)
+    })
+  }, [entrySection])
+
   return (
 
-    <a href='#home' className='relative flex w-full h-full justify-end items-center'>{title}</a>
+    <a href={`#${id}`} ref={link} className={`relative flex w-full h-full justify-end items-center uppercase pr-2 text-[${color}] font-bold text-sm font-Crimson_Pro`}>{title}</a>
 
   )
 }
--- src/components/react/MenuNavbar.jsx
+++ src/components/react/MenuNavbar.jsx
@@ -1,15 +1,36 @@
 import LinkMenu from './LinkMenu'
 
 const MenuNavbar = () => {
-  const links = ['home', 'about', 'skills', 'projects', 'contact']
+  const links = [
+    {
+      id: 'home',
+      title: 'home'
+    },
+    {
+      id: 'about',
+      title: 'about'
+    },
+    {
+      id: 'skills',
+      title: 'skills'
+    },
+    {
+      id: 'projects',
+      title: 'projects'
+    },
+    {
+      id: 'contact',
+      title: 'contact'
+    }
+  ]
 
   return (
     <div className='fixed z-50 right-0 bottom-0 w-40 h-48 pr-8'>
-      <nav className='relative w-full h-full border-r-2 border-[#4E0601] flex'>
-        <ul className='flex flex-grow flex-col justify-between'>
+      <nav className='relative w-full h-full border-r-4 border-black flex pb-1 dark:border-[#4E0601]'>
+        <ul className='relative right-8 flex flex-grow flex-col justify-between gap-1'>
           {
             links.map((el, index) => (
-              <li key={index} className='bg-red-400 flex-grow text-right pr-4'> <LinkMenu title={el} /></li>
+              <li key={index} className='relative flex-grow text-right before:content-[""] before:absolute before:w-8 before:h-[3px] before:bg-black before:top-1/2 dark:before:bg-[#4E0601]'> <LinkMenu id={el.id} title={el.title} /></li>
             ))
           }
         </ul>
