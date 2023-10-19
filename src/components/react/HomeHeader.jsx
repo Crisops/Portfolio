be1e70f000cfb84e93241d8c878fa4989bd7f477
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-10-18 23:06:31 -0500

    Se añaden redes sociales

--- src/components/react/HomeHeader.jsx
+++ src/components/react/HomeHeader.jsx
@@ -1,5 +1,5 @@
-import ButtonLanguage from './ButtonLanguage'
 import ButtonTheme from './ButtonTheme'
+import SocialNetworks from './SocialNetworks'
 
 const HomeHeader = () => {
   const logo = '<Cs/>'
@@ -7,8 +7,8 @@ const HomeHeader = () => {
   return (
     <header className='fixed top-0 left-0 py-5 px-20 w-full flex justify-between items-center backdrop-blur-sm z-50'>
       <span className='animate-fade-right animate-once animate-duration-500 animate-normal text-black font-Crimson_Pro font-bold text-5xl transition-colors duration-300 dark:text-white'>{logo}</span>
-      <div className='animate-fade-left animate-once animate-duration-500 flex gap-4'>
-        <ButtonLanguage />
+      <div className='animate-fade-left animate-once animate-duration-500 flex flex-col gap-4 justify-center items-center'>
+        <SocialNetworks />
         <ButtonTheme />
       </div>
     </header>
---
+++ src/components/react/SocialNetworks.jsx
@@ -0,0 +1,13 @@
+import { IconGitGubSocial, IconInstagram, IconLinkInd } from './Icons'
+
+const SocialNetworks = () => {
+  return (
+    <div className='relative flex gap-4  [&>a>svg]:transition-colors [&>a>svg]:hover:cursor-pointer dark:[&>a>svg>path]:fill-white dark:[&>a>svg>g]:fill-white'>
+      <IconGitGubSocial url='https://github.com/Crisops' />
+      <IconInstagram url='https://instagram.com/cris.alejo13' />
+      <IconLinkInd url='https://www.linkedin.com/in/cristian-perez-5b306a239/' />
+    </div>
+  )
+}
+
+export default SocialNetworks
