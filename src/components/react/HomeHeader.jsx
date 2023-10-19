60a407f61c74ba457c78133dc0c9d89d9d71f295
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-10-19 13:49:58 -0500

    Se cambia de posición redes sociales

--- src/components/react/HomeHeader.jsx
+++ src/components/react/HomeHeader.jsx
@@ -1,5 +1,4 @@
 import ButtonTheme from './ButtonTheme'
-import SocialNetworks from './SocialNetworks'
 
 const HomeHeader = () => {
   const logo = '<Cs/>'
@@ -8,7 +7,6 @@ const HomeHeader = () => {
     <header className='fixed top-0 left-0 py-5 px-20 w-full flex justify-between items-center backdrop-blur-sm z-50'>
       <span className='animate-fade-right animate-once animate-duration-500 animate-normal text-black font-Crimson_Pro font-bold text-5xl transition-colors duration-300 dark:text-white'>{logo}</span>
       <div className='animate-fade-left animate-once animate-duration-500 flex flex-col gap-4 justify-center items-center'>
-        <SocialNetworks />
         <ButtonTheme />
       </div>
     </header>
