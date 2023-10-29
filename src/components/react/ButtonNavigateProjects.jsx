309f9b4ee09ae8b040035a7ec3fc640dbc36c4e7
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-10-28 22:22:31 -0500

    Se añade animación  en Home y se arreglan algunas cosas en las Cards

--- src/components/react/ButtonNavigateProjects.jsx
+++ src/components/react/ButtonNavigateProjects.jsx
@@ -1,6 +1,6 @@
 const ButtonNavigateProjects = ({ url, bgColor, title, icon }) => {
   return (
-    <a href={url} className={`w-24 h-10 rounded ${bgColor} transition-colors pointer-events-auto hover:bg-opacity-90 dark:hover:bg-opacity-90 dark:first:bg-zinc-700 dark:last:bg-[#FA4003]`} target='_blank' rel='noreferrer'>
+    <a href={url} className={`w-24 h-10 rounded ${bgColor} transition-colors duration-300 pointer-events-auto hover:bg-opacity-90 dark:hover:bg-opacity-90 dark:first:bg-black dark:last:bg-[#FA4003]`} target='_blank' rel='noreferrer'>
       <span className='font-Hind_Vadodara text-xs font-bold text-white uppercase'>{title}</span>
       {icon}
     </a>
--- src/components/react/InfoMe.jsx
+++ src/components/react/InfoMe.jsx
@@ -1,15 +1,43 @@
 import AnimateImg from './AnimateImg'
 import HomeHeader from './HomeHeader'
 import SectionAbout from './SectionAbout'
+import TypeIt from 'typeit-react'
 
 const InfoMe = ({ buttons }) => {
+  const handleTypeIt = (instancie) => {
+    instancie
+      .type('¿Porque', { delay: 500 })
+      .move(-3)
+      .type(' ')
+      .move(null, { to: 'END' })
+      .pause(500)
+      .delete(1)
+      .type('é')
+      .type(' dejar ideas en el aire')
+      .pause(500)
+      .move(-17)
+      .type(' tus')
+      .move(null, { to: 'END', instant: true })
+      .type(' cuando puedes hacerlas realidad?')
+    return instancie
+  }
+
   return (
     <>
       <section id='home' className='relative w-screen h-screen bg-white grid grid-cols-2 dark:bg-[#0F0101]'>
         <HomeHeader />
         <div className='flex gap-9 flex-col justify-center pl-20'>
-          <h1 className='text-9xl font-Crimson_Pro font-extrabold uppercase mb-4 text-transparent bg-clip-text bg-gradient-to-br from-[#FFB0B1] via-[#FF6567] to-[#FF0006] transition-colors duration-300 dark:bg-gradient-to-br dark:from-[#290102]  dark:via-[#A50002 ] dark:to-[#FF030B]'>Cristian Alejandro</h1>
-          <p className='text-xl text-gray-800 font-Hind_Guntur font-light max-w-lg transition-colors duration-300 dark:text-gray-200'>¿Por qué dejar tus ideas en el aire cuando puedes hacerlas realidad?</p>
+          <h1 className='animate-fade-right animate-duration-500 animate-ease-in text-9xl font-Crimson_Pro font-extrabold uppercase mb-4 text-transparent bg-clip-text bg-gradient-to-br from-[#FFB0B1] via-[#FF6567] to-[#FF0006] dark:bg-gradient-to-br dark:from-[#290102]  dark:via-[#A50002 ] dark:to-[#FF030B]'>Cristian Alejandro</h1>
+          <TypeIt
+            options={{
+              speed: 30,
+              startDelay: 600,
+              waitUntilVisible: true
+            }}
+            className='text-xl text-zinc-700 font-Hind_Guntur font-light max-w-lg transition-colors duration-300 dark:text-zinc-200'
+            as='p'
+            getBeforeInit={handleTypeIt}
+          />
           {buttons}
         </div>
         <AnimateImg />
