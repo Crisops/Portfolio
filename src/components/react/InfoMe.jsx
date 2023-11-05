12ab08b1a3e110682b339087e43a48a464d01efd
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-11-05 18:57:14 -0500

    Se corrige media query en 1280px

--- src/components/react/InfoMe.jsx
+++ src/components/react/InfoMe.jsx
@@ -28,8 +28,8 @@ const InfoMe = ({ buttons }) => {
       <section id='home' className='relative w-screen h-screen grid grid-cols-2 max-xl:grid-cols-none  max-xl:grid-rows-2 bg-white dark:bg-[#0F0101]'>
         <HomeHeader />
         <GridBgHome />
-        <div className='flex gap-9 flex-col max-xl:justify-end max-[1024px]:justify-center max-sm:justify-center max-[1024px]:items-center pl-10 max-md:px-0 justify-center'>
-          <h1 className='max-[1850px]:text-[7.5rem] max-[1750px]:text-[7rem] max-[1652px]:text-[6.5rem] max-[1652px]:text-8xl max-[1024px]:text-center max-[1536px]:mb-0 max-[1536px]:text-[6.3rem] max-[1280px]:text-[5.6rem] max-xl:max-w-2xl max-lg:text-8xl max-md:text-[5rem] max-[574px]:text-[4.5rem] max-[510px]:text-6xl max-[430px]:text-[3rem] max-[400px]:text-[3rem] max-sm:mt-8 animate-fade-right animate-duration-500 animate-ease-in text-9xl font-Crimson_Pro font-extrabold uppercase mb-4 text-transparent bg-clip-text bg-gradient-to-br from-[#FFB0B1] via-[#FF6567] to-[#FF0006] dark:bg-gradient-to-br dark:from-[#290102]  dark:via-[#A50002 ] dark:to-[#FF030B]'>Cristian Alejandro</h1>
+        <div className='flex gap-9 flex-col max-xl:justify-end max-[1024px]:justify-center max-sm:justify-center max-[1024px]:items-center max-[1279px]:items-center pl-10 max-md:px-0 justify-center'>
+          <h1 className='max-[1850px]:text-[7.5rem] max-[1750px]:text-[7rem] max-[1652px]:text-[6.5rem] max-[1652px]:text-8xl max-[1024px]:text-center max-[1536px]:mb-0 max-[1536px]:text-[6.3rem] max-[1280px]:text-[5.6rem] max-[1279px]:text-center max-xl:max-w-2xl max-lg:text-8xl max-md:text-[5rem] max-[574px]:text-[4.5rem] max-[510px]:text-6xl max-[430px]:text-[3rem] max-[400px]:text-[3rem] max-sm:mt-8 animate-fade-right animate-duration-500 animate-ease-in text-9xl font-Crimson_Pro font-extrabold uppercase mb-4 text-transparent bg-clip-text bg-gradient-to-br from-[#FFB0B1] via-[#FF6567] to-[#FF0006] dark:bg-gradient-to-br dark:from-[#290102]  dark:via-[#A50002 ] dark:to-[#FF030B]'>Cristian Alejandro</h1>
           <div className='relative z-40'>
             <TypeIt
               options={{
@@ -37,7 +37,7 @@ const InfoMe = ({ buttons }) => {
                 startDelay: 600,
                 waitUntilVisible: true
               }}
-              className='text-xl 2xl:text-xl xl:text-lg max-[1024px]:text-center max-[648px]:text-base max-[648px]:max-w-sm text-zinc-700 font-Hind_Guntur font-light max-w-lg transition-colors duration-300 dark:text-zinc-200 z-50'
+              className='text-xl 2xl:text-xl max-[1279px]:text-center xl:text-lg max-[1024px]:text-center max-[648px]:text-base max-[648px]:max-w-sm text-zinc-700 font-Hind_Guntur font-light max-w-lg transition-colors duration-300 dark:text-zinc-200 z-50'
               as='p'
               getBeforeInit={handleTypeIt}
             />
