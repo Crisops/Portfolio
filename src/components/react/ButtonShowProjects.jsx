cef1e9e3cb09675901160fee035c2a3bc69c3c7b
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-11-07 17:33:34 -0500

    Se arregla bug en botón ver más projectos

--- src/components/react/ButtonShowProjects.jsx
+++ src/components/react/ButtonShowProjects.jsx
@@ -1,11 +1,10 @@
 const ButtonShowProjects = ({ handleMoreProject, viewMore }) => {
   return (
-    <a
-      href='#projects'
+    <button
       onClick={handleMoreProject} className={`absolute left-1/2 -translate-x-1/2 ${viewMore ? 'bottom-4' : 'bottom-20'} px-7 py-3
    ${viewMore ? 'bg-zinc-950 dark:bg-[#770000]' : 'bg-[#FF0000] dark:bg-[#210000]'}  rounded-md text-white font-bold font-Hind_Guntur uppercase transition-colors duration-300 pointer-events-auto z-[11] ${viewMore ? 'hover:bg-opacity-95 dark:hover:bg-opacity-75' : 'hover:bg-opacity-75 dark:hover:bg-opacity-75'} `}
     >{viewMore ? 'Ocultar' : 'Ver más'}
-    </a>
+    </button>
   )
 }
 
