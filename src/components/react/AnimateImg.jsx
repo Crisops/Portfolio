4bcabc395cefdedfe943048c72bc7e7d9c8ab4a0
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-11-07 13:36:26 -0500

    Cambio url Img Project

--- src/components/react/AnimateImg.jsx
+++ src/components/react/AnimateImg.jsx
@@ -12,7 +12,7 @@ const AnimateImg = () => {
   return (
     <div className='relative'>
       <div className='relative top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-[60%] h-[60%] max-[1536px]:w-[60%] max-[1536px]:h-[60%] max-[1450px]:h-[50%] max-[1279px]:w-[40%] max-[1279px]:h-[90%] max-[1024px]:w-[60%] max-[1024px]:h-[75%] max-[768px]:w-[50%] max-sm:w-[75%] max-sm:h-[80%] grid grid-cols-16 auto-rows-[50px]'>
-        <div ref={image} id='cub' className='absolute animate-fade animate-once animate-duration-1000 animate-alternate w-full h-full bg-[url("/img/me.webp")] bg-cover bg-center bg-no-repeat'></div>
+        <div ref={image} id='cub' className='absolute animate-fade animate-once animate-duration-1000 animate-alternate w-full h-full bg-[url("Portfolio/img/me.webp")] bg-cover bg-center bg-no-repeat'></div>
       </div>
     </div>
   )
